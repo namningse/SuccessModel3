272ad3fe3e11e024ec66d93d5bf5269c40a8daee
Chaow Porkaew <pchaowmobile@gmail.com>
2014-12-17 12:39:06 +0700

    add local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('chaow-pc'),
+	'local' => array('chaow-pc','chaows-MacBook-Pro.local'),
 
 ));
 
